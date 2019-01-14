6aa067f5ae368b21e541089d4f63e8a3d460048d
Eugene S <simakin.eugene@gmail.com>
2019-01-14 20:49:55 +0300

    [4][lexer] new bitwise and relational operators + tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -11,10 +11,24 @@ pub enum Token {
     ConstInt(u32),
     Negation,
     BitwiseComplement,
+    BitwiseOr,
+    BitwiseAnd,
+    BitwiseXor,
+    BitwiseShiftLeft,
+    BitwiseShiftRight,
+    Modulo,
     LogicalNegation,
     Addition,
     Multiplication,
     Division,
+    Equal,
+    NotEqual,
+    Or,
+    And,
+    LessThan,
+    LessOrEqual,
+    GreaterThan,
+    GreaterOrEqual,
 }
 
 pub fn tokenize(contents: &str) -> Vec<Token> {
@@ -29,7 +43,8 @@ pub fn tokenize(contents: &str) -> Vec<Token> {
             ')' => tokens.push(Token::CloseParenthesis),
             ';' => tokens.push(Token::Semicolon),
             '-' => tokens.push(Token::Negation),
-            '!' => tokens.push(Token::LogicalNegation),
+            '^' => tokens.push(Token::BitwiseXor),
+            '%' => tokens.push(Token::Modulo),
             '~' => tokens.push(Token::BitwiseComplement),
             '+' => tokens.push(Token::Addition),
             '*' => tokens.push(Token::Multiplication),
@@ -76,6 +91,68 @@ pub fn tokenize(contents: &str) -> Vec<Token> {
                     } else {
                         tokens.push(Token::Identifier(keyword_or_identifier));
                     }
+                } else if ch == '<' || ch == '>' || ch == '=' || ch == '|' || ch == '&' || ch == '!'
+                {
+                    let mut skip = false;
+                    {
+                        let peek = chars.peek();
+                        match ch {
+                            '<' => match peek {
+                                Some(&'=') => {
+                                    tokens.push(Token::LessOrEqual);
+                                    skip = true;
+                                }
+                                Some(&'<') => {
+                                    tokens.push(Token::BitwiseShiftLeft);
+                                    skip = true;
+                                }
+                                _ => tokens.push(Token::LessThan),
+                            },
+                            '>' => match peek {
+                                Some(&'=') => {
+                                    tokens.push(Token::GreaterOrEqual);
+                                    skip = true;
+                                }
+                                Some(&'>') => {
+                                    tokens.push(Token::BitwiseShiftRight);
+                                    skip = true;
+                                }
+                                _ => tokens.push(Token::GreaterThan),
+                            },
+                            '=' => match peek {
+                                Some(&'=') => {
+                                    tokens.push(Token::Equal);
+                                    skip = true;
+                                }
+                                _ => (),
+                            },
+                            '|' => match peek {
+                                Some(&'|') => {
+                                    tokens.push(Token::Or);
+                                    skip = true;
+                                }
+                                _ => tokens.push(Token::BitwiseOr),
+                            },
+                            '&' => match peek {
+                                Some(&'&') => {
+                                    tokens.push(Token::And);
+                                    skip = true;
+                                }
+                                _ => tokens.push(Token::BitwiseAnd),
+                            },
+                            '!' => match peek {
+                                Some(&'=') => {
+                                    tokens.push(Token::NotEqual);
+                                    skip = true;
+                                }
+                                _ => tokens.push(Token::LogicalNegation),
+                            },
+                            _ => (),
+                        }
+                    }
+                    if skip {
+                        chars.next();
+                    }
                 }
             }
         }
@@ -89,20 +166,21 @@ pub fn tokenize(contents: &str) -> Vec<Token> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::Token::*;
 
     #[test]
     fn tokenize_simple() {
         let result = tokenize("int main() {\n return 3; \n}");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::ReturnKeyword,
-            Token::ConstInt(3),
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            ConstInt(3),
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
@@ -111,14 +189,14 @@ mod tests {
     fn tokenize_min_spaces() {
         let result = tokenize("int main(){return;}");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::ReturnKeyword,
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
@@ -127,15 +205,15 @@ mod tests {
     fn tokenize_spaces() {
         let result = tokenize("   int    main ( ) {   return    100  ; }");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::ReturnKeyword,
-            Token::ConstInt(100),
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            ConstInt(100),
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
@@ -144,18 +222,18 @@ mod tests {
     fn tokenize_unary_ops() {
         let result = tokenize("int main() { return !~-4; }");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::ReturnKeyword,
-            Token::LogicalNegation,
-            Token::BitwiseComplement,
-            Token::Negation,
-            Token::ConstInt(4),
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            LogicalNegation,
+            BitwiseComplement,
+            Negation,
+            ConstInt(4),
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
@@ -164,14 +242,14 @@ mod tests {
     fn tokenize_no_space_at_retval() {
         let result = tokenize("int main() { return0; }");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::Identifier(String::from("return0")),
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            Identifier(String::from("return0")),
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
@@ -180,15 +258,77 @@ mod tests {
     fn tokenize_return_uppercase() {
         let result = tokenize("int main() { RETURN 3; }");
         let expected: Vec<Token> = vec![
-            Token::IntKeyword,
-            Token::Identifier(String::from("main")),
-            Token::OpenParenthesis,
-            Token::CloseParenthesis,
-            Token::OpenBrace,
-            Token::Identifier(String::from("RETURN")),
-            Token::ConstInt(3),
-            Token::Semicolon,
-            Token::CloseBrace,
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            Identifier(String::from("RETURN")),
+            ConstInt(3),
+            Semicolon,
+            CloseBrace,
+        ];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn tokenize_bitwise() {
+        let result = tokenize("int main() { return <<>>100; }");
+        let expected: Vec<Token> = vec![
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            BitwiseShiftLeft,
+            BitwiseShiftRight,
+            ConstInt(100),
+            Semicolon,
+            CloseBrace,
+        ];
+        assert_eq!(result, expected);
+        let result = tokenize("int main() { return ^&|1; }");
+        let expected: Vec<Token> = vec![
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            BitwiseXor,
+            BitwiseAnd,
+            BitwiseOr,
+            ConstInt(1),
+            Semicolon,
+            CloseBrace,
+        ];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn tokenize_relational() {
+        let result = tokenize("int main() { return 1 < 2 <= 3 == 3 >= 2 > 1; }");
+        let expected: Vec<Token> = vec![
+            IntKeyword,
+            Identifier(String::from("main")),
+            OpenParenthesis,
+            CloseParenthesis,
+            OpenBrace,
+            ReturnKeyword,
+            ConstInt(1),
+            LessThan,
+            ConstInt(2),
+            LessOrEqual,
+            ConstInt(3),
+            Equal,
+            ConstInt(3),
+            GreaterOrEqual,
+            ConstInt(2),
+            GreaterThan,
+            ConstInt(1),
+            Semicolon,
+            CloseBrace,
         ];
         assert_eq!(result, expected);
     }
